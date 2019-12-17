9712b8a1b110385757bdbea1903220d90bad31c8
yq <nyq1006gmail.com>
2019-12-17 10:08:09 +0800

    课堂作业——举例&& 鸡兔同笼

--- DstaStructuresAndAlgorithm_/Program.cs
+++ DstaStructuresAndAlgorithm_/Program.cs
@@ -7,7 +7,8 @@ namespace DstaStructuresAndAlgorithm_
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            //鸡兔同笼
+            //课堂作业——举例
         }
     }
 }
